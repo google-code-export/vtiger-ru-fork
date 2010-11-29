7ec7e8c22a49143d6a90ef759b74521f38a130c3
Eugene Babiy <Eugene Babiy@localhost>
2010-11-29 23:25:22 +0200

    ???????? ??? ?????????? ? ?????????? ?????? PDFSettings. ????????? ?????????? TCPDF ?? ?????? PDFSettings.

---
+++ modules/PDFSettings/module_manager_file/Pdfsettings/modules/Pdfsettings/language/ge_de.lang.php
@@ -0,0 +1,29 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+//common
+'Pdfsettings' => 'PDF Einstellungen',
+'LBL_MODULE_NAME' => 'PDF Einstellungen',
+'LBL_SETTINGS' => 'Einstellungen',
+'LBL_PDF_CONFIGURATOR'=>'PDF Konfigurator',
+
+'LBL_PDFCONFIGURATOR' =>'PDF Konfigurator Einstellungen',
+'LBL_PDFCONFIGURATOR_SHORT' =>'PDF Einstellungen',
+'LBL_PDFCONFIGURATOR_DESCRIPTION' =>'formatieren Sie PDF Ausgaben für Angebote, Bestellungen und Rechungen',
+'LBL_PDFCONFIGURATOR_ENABLE' =>'für Nutzer sperren',
+'LBL_PDFCONFIGURATOR_QUOTES' =>'Einstellungen für Angebote',
+'LBL_PDFCONFIGURATOR_INVOICES' =>'Einstellungen für Rechnungen',
+'LBL_PDFCONFIGURATOR_SO' =>'Einstellungen für Bestellbestätigungen',
+'LBL_PDFCONFIGURATOR_PO' =>'Einstellungen für Einkaufsbestellungen',
+
+);
+
+?>
