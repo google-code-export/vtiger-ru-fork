d19b6ce6ebcd94a541233dd5588143e61d8a4f90
Eugene Babiy <Eugene Babiy@localhost>
2010-12-16 13:44:43 +0200

    ???????? ????????? ?????.

--- trunk/rucrm/include/js/ru_ru.lang.js
+++ trunk/rucrm/include/js/ru_ru.lang.js
@@ -315,8 +315,8 @@ var alert_arr = {
 	ITEM_GROUP_REQUIRED_SO:'For the group tax mode at sales orders you need to select at least one column content for the display of items.',
 	ITEM_INDIVIDUAL_REQUIRED_INV:'For the individual tax mode at invoices you need to select at least one column content for the display of items.',
 	ITEM_GROUP_REQUIRED_INV:'For the group tax mode at invoices you need to select at least one column content for the display of items.',
-	TAB_GENERAL:'General',
-	TAB_GROUP:'group Tax',
-	TAB_INDIVIDUAL:'individual Tax'
+	TAB_GENERAL:'Общее',
+	TAB_GROUP:'групповой Налог',
+	TAB_INDIVIDUAL:'индивидуальный Налог'
 	};
 
